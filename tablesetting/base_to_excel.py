d73e689dcbb16243608a33626d77966639705549
Msolis314 <solisfanny2021@gmail.com>
2023-11-30 10:47:10 -0600

    funcion para conertir

--- tablesetting/base_to_excel.py
+++ tablesetting/base_to_excel.py
@@ -1,22 +1,32 @@
+"""Modulo para pasar los datos a excel
+"""
 import sqlite3 , os , openpyxl
 from sqlite3 import Error
+import system_vars.config as config
 import pandas as pd
-conn = sqlite3.connect('Mari.db')  
-cursor = conn.cursor()
-book = openpyxl.Workbook()
-sheet = book.active
 
 
-consulta = "SELECT * FROM ingresos"
-cursor.execute(consulta)
-results = cursor.fetchall()
-i = 0
-for row in results:
-  i += 1
-  j = 1
-  for col in row:
-    cell = sheet.cell(row = i, column = j)
-    cell.value = col
-    j += 1
-book.save("excel.xlsx")
-conn.close()
+def data_to_excel(nombre_tabla):
+  """Funcion para cargar una tabla de la base de datos a un archivo excel 
+
+  :param nombre_tabla: nombre de la tabla en la base de datos, ingresos, presupuesto o gastos
+  :type nombre_tabla: str
+  """
+  current_database = f'{config.currentuser}.db'
+  conn = sqlite3.connect(current_database)  
+  cursor = conn.cursor()
+  book = openpyxl.Workbook()
+  sheet = book.active
+  consulta = f"SELECT * FROM {nombre_tabla}"
+  cursor.execute(consulta)
+  results = cursor.fetchall()
+  i = 0
+  for row in results:
+    i += 1
+    j = 1
+    for col in row:
+      cell = sheet.cell(row = i, column = j)
+      cell.value = col
+      j += 1
+  book.save(f"{nombre_tabla}.xlsx")
+  conn.close()
